ec83eac10826f4c9b243c7fadb0112118f1c452e
Urban427 <urbanzak21@gmail.com>
2026-08-08 23:13:31 +0300

    fixed merged scritps

--- tools/build.sh
+++ tools/build.sh
@@ -2,20 +2,6 @@
 set -e
 
 PLATFORM=${1:-windows}
-<<<<<<< HEAD
-ROOT_DIR=$(realpath "$(dirname "$0")/..")
-DOCKER_BUILD_DIR="/workspace"
-BUILD_DIR="$ROOT_DIR/build"
-
-echo "Building for $PLATFORM..."
-
-rsync -a --delete --exclude 'build' --exclude '.git', --exclude '.CMakeFiles' "$ROOT_DIR/" "$DOCKER_BUILD_DIR/"
-
-cmake --preset "$PLATFORM" -S "$DOCKER_BUILD_DIR" -B "$DOCKER_BUILD_DIR/.CMakeFiles"
-cmake --build "$DOCKER_BUILD_DIR/.CMakeFiles"
-
-rsync -a --delete "$DOCKER_BUILD_DIR/build/" "$BUILD_DIR/"
-=======
 PROJECT_PATH=${2}
 ENGINE_PATH=${3}
 BUILD_PATH=${4}
@@ -50,6 +36,5 @@ cmake --build "$DOCKER_BUILD_DIR/.CMakeFiles"
 rsync -a --delete \
     "$DOCKER_BUILD_DIR/build/" \
     "$BUILD_PATH/"
->>>>>>> 561d111 (fixed compilation scritps)
 
 echo "Build finished."
